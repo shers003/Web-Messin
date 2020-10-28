b9724e10178120e10551da0f0cdfbd36ca056172
sonny sheridan <sonny.sheridan@yahoo.co.uk>
2020-10-28 00:04:13 +0000

    added mongo-store added validPassword method to userSchema, need to fix authentication

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const env = require('dotenv').config();
 const passport = require('passport');
 const session = require('express-session');
 const loaclStrategy = require('passport-local').Strategy;
+const MongoStore = require('connect-mongo')(session);
 
 const app = express();
 const port = 3000;
 const uri = 'mongodb://localhost:27017/messinDB'
+const connection = mongoose.createConnection(uri,{useNewUrlParser:true, useUnifiedTopology:true});
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('static'));
@@ -19,19 +21,25 @@ app.set('view engine', 'ejs');
 app.use(passport.initialize());
 app.use(passport.session());
 
+// took inspiration from https://levelup.gitconnected.com/everything-you-need-to-know-about-the-passport-local-passport-js-strategy-633bbab6195
+const sessionstore = new MongoStore({mongooseConnection: connection, collection:'sessions'})
+
 //creating user login session
 const sess = {
   name:'userSess',
   secret: process.env.SESSIONSECRET,
   resave:false,
-  saveUninitialized:false
+  saveUninitialized:false,
+  store:sessionstore
 };
 app.use(session(sess));
 
 passport.serializeUser((user,done)=>{
+  console.log('serializeUser '+ user);
   done(null, user.id);
 });
 passport.deserializeUser((id,done)=>{
+  console.log('deserializeUser '+ id);
   User.findById(id,(err,user)=>{
     done(err,user);
   });
@@ -41,16 +49,20 @@ passport.use(new loaclStrategy((username, password, cb)=>{
   User.findOne({username:username},(err,user)=>{
     if(!err){
       if(user){
-        if(user.validPassword(password)){
+        if(user.validPassword(user,password)){
+          console.log('User was authenticated in the strategy');
           return cb(null,user)
         }else{
+          console.log('Password Incorrect');
           return cb(null,false, {message:'Incorrect password'})
         }
       }else{
+        console.log('No user found in strategey');
         //note see if these actually need to be retuerned possible error
         return cb(null, false, {message:'My Man don\'t even exist'})
       }
     }else{
+      console.log(err);
       return cb(err)
     }
   });
@@ -71,18 +83,23 @@ const userSchema = new mongoose.Schema({
   username:String,
   password:String
 });
+userSchema.methods.validPassword = (user,pwd)=>{
+  const userPwd = user.password;
+  const pass = bcrypt.compareSync(pwd, userPwd);
+  return pass;
+};
 
 const User = new mongoose.model('user',userSchema);
+
 
 
 //routing
 app.get('/',(req,res)=>{
     res.render('index');
-    console.log(req.session);
 });
 
 app.route('/register') .get((req,res)=>{
-  console.log('yep');
+  console.log('User sent get request to /register');
   res.render('register');
 }) .post((req,res)=>{
   let saltrounds = 10
@@ -94,35 +111,47 @@ app.route('/register') .get((req,res)=>{
     password:password
   });
 
-  user.save((err)=>{
+  User.findOne({username:username}, (err, user)=>{
     if(!err){
-      console.log(user);
-      res.send('u had been saved')
-    }else{console.log(err);}
+      if(!user){
+        user.save((err)=>{
+          if(!err){
+            console.log('This user is now save '+user);
+            res.send('u had been saved')
+          }else{console.log(err);}
+        });
+      }else{
+        console.log('User already exists');
+        res.redirect('/register')
+      }
+    }else{
+      console.log(err);
+      res.redirect('/register')
+    }
   });
+
 });
 
 app.route('/login') .get((req,res)=>{
-  res.send('who u blud?')
-}) .post((req,res)=>{
-  saltrounds = 10;
-  let username = req.body.username;
-  let password = req.body.password;
+  if(req.isAuthenticated()){
+    res.send('Go on lad')
+    console.log('was authenticated in /login get');
+  }else{
+    res.render('login');
+  }
 
-  User.findOne({username:username}, (err, user)=>{
-    if(!err){
-      if(user){
-        let check = bcrypt.compareSync(req.body.password,user.password);
-        if(check){
-          res.send('My g')
-        }else{res.send('Aye don\'t be gettin me MAD!!');}
-      }else{res.send('I don\'t know u still!')}
-    }else{console.log(err);}
-  });
-});
+}) .post(passport.authenticate('local',{failureRedirect:'/'}),(req,res)=>{
+  console.log('User was authenticated in /login post');
+  res.send('Go on lad')
+})
 
-app.route('/post') .get((req,res)=>{
-  res.send('well be a page obvs');
+app.route('/Shoppingcart') .get((req,res)=>{
+  if(req.isAuthenticated()){
+    res.render('shoppingCart')
+    console.log('was authenticated in /Shoppingcart get');
+  }else{
+    res.render('login');
+  }
 }) .post((req,res)=>{
 
 
